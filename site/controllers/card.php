48cb8d38fdee84e2e4ffca8e51350224fea3cd19
Jorge Fco. <jorge.fco.mend@gmail.com>
2024-09-25 09:56:38 -0600

    Update controller

--- site/controllers/card.php
+++ site/controllers/card.php
@@ -1,6 +1,46 @@
 <?php
 	/*
-		🗓 Date: 23-12-2024
+		🗓 Date: 25-12-2024
 		👨🏻‍💻 Developer by Jorge Fco.™
 	*/
+
+	return function ($page) {
+
+		//= POST
+		if (r::is('post') == true) {
+			// Set
+			$name = $page;
+			$firstname = $page;
+			$lastname = $page;
+			$position = $page;
+			$email = $page;
+			$phone = $page;
+			$whatsapp = $page;
+			$company = $page;
+			$address = $page;
+			$website = $page;
+
+			//= ARRAY
+			$data = array(
+				// 📋 User
+				'name' => $name,
+				'firstname' => $firstname,
+				'lastname' => $lastname,
+				'position' => $position,
+				'email' => $email,
+				'phone' => $phone,
+				'whatsapp' => $whatsapp,
+
+				// 🏢 Business
+				'company' => $company,
+				'address' => $address,
+				'website' => $website
+			);
+		}
+
+		// Set template
+		return [
+			//
+		];
+	}
 ?>
